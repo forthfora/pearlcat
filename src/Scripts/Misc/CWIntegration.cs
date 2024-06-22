f4c86bc9aba7767f6f914be7734a44fa0b55a877
forthbridge <79264743+forthbridge@users.noreply.github.com>
2024-06-22 19:06:59 +0800

    More Dialogue + Fixes

--- src/Scripts/Misc/CWIntegration.cs
+++ src/Scripts/Misc/CWIntegration.cs
@@ -76,5 +76,54 @@ public static class CWIntegration
     private static void SSOracleBehavior_SpecialEvent(On.SSOracleBehavior.orig_SpecialEvent orig, SSOracleBehavior self, string eventName)
     {
         orig(self, eventName);
+
+        if (eventName == "PEARLCAT_GIVE_CW")
+        {
+            GiveCWPearl(self.oracle);
+        }
+    }
+
+    private static void GiveCWPearl(Oracle oracle, bool withEffect = true)
+    {
+        foreach (var roomObject in oracle.room.physicalObjects)
+        {
+            foreach (var physicalObject in roomObject)
+            {
+                if (physicalObject is not Player player) continue;
+
+                if (!player.TryGetPearlcatModule(out var playerModule)) continue;
+
+
+                var abstractPearl = new DataPearl.AbstractDataPearl(oracle.room.world, AbstractPhysicalObject.AbstractObjectType.DataPearl, null, player.abstractCreature.pos, oracle.room.game.GetNewID(), -1, -1, null, Enums.Pearls.CW_Pearlcat);
+
+                if (playerModule.Inventory.Count >= ModOptions.MaxPearlCount.Value)
+                {
+                    abstractPearl.pos = player.abstractCreature.pos;
+                    oracle.room.abstractRoom.AddEntity(abstractPearl);
+                    abstractPearl.RealizeInRoom();
+
+                    var freeHand = player.FreeHand();
+
+                    if (freeHand > -1)
+                    {
+                        player.SlugcatGrab(abstractPearl.realizedObject, freeHand);
+                    }
+                }
+                else
+                {
+                    player.StoreObject(abstractPearl);
+
+                    if (withEffect)
+                    {
+                        playerModule.ShowHUD(120);
+                    }
+                }
+
+                if (withEffect)
+                {
+                    oracle.room.PlaySound(SoundID.SS_AI_Give_The_Mark_Boom, physicalObject.firstChunk, false, 1.5f, 0.5f);
+                }
+            }
+        }
     }
 }
