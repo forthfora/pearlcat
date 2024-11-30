ead4f50a5b7fbb98a544f62dd04412a9701108b1
forthfora <forthfora@gmail.com>
2024-11-30 11:10:01 +0800

    1.3.4 Menu Ctor Null Fix

--- src/Hooks/Menu/Menu_Hooks.cs
+++ src/Hooks/Menu/Menu_Hooks.cs
@@ -119,6 +119,11 @@ public static class Menu_Hooks
     {
         orig(self, menu, owner, sceneID);
 
+        if (sceneID is null)
+        {
+            return;
+        }
+
         if (!sceneID.value.Contains("Pearlcat"))
         {
             return;
