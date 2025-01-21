31a3edcb3e26213f622a533dfe282604edd8c7b1
forthfora <forthfora@gmail.com>
2025-01-21 17:39:39 +0800

    1.4.0

--- src/Hooks/Menu/Menu_Helpers.cs
+++ src/Hooks/Menu/Menu_Helpers.cs
@@ -873,7 +873,7 @@ public static class Menu_Helpers
 
             if (type == IllustrationType.PearlActive)
             {
-                i.SetPosition(685, 600);
+                i.SetPosition(685, 630);
                 i.SetDepth(2.3f);
                 i.LayerInFrontOf("pearlcathead");
                 return;
